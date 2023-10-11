505656323c03b97969c9fa14030958aa8b9732ea
washupo <paulinewashukenyi@gmail.com>
2023-10-11 12:24:35 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import path from "path";
+import { resolve } from "path";
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	base: '/restaurant-css-framework/',
+	  root: path.resolve(__dirname),
+  	build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+      },
+    },
+	server: {
+    port: 8080,
+    hot: true
+	},
+  },
+})
+
