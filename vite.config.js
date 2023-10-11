b93fda8055e5afd47f55e844bcfc64257f319c0c
washupo <paulinewashukenyi@gmail.com>
2023-10-12 01:53:22 +0200

    HTML/CSS v1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/restaurant-css-framework/',
+	base: '/tim-berners-lee/',
 	  root: path.resolve(__dirname),
   	build: {
     outDir: "../dist",
